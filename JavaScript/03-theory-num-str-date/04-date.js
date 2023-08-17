c76b3f7f70e814f744559e70fc3213a06989cf0b
Vladimir <komlevladimir@gmail.com>
2023-08-18 00:06:51 +0300

    add: 04-date.js

--- JavaScript/03-theory-num-str-date/04-date.js
+++ JavaScript/03-theory-num-str-date/04-date.js
@@ -28,10 +28,46 @@ console.log(now.toLocaleTimeString()); // 23:13:49
 
 // ======= AppTime
 
+let mode = 'time';
 const now = new Date();
 const output = document.getElementById('output');
 const fullBtn = document.getElementById('full');
 const dateBtn = document.getElementById('date');
 const timeBtn = document.getElementById('time');
 
-output.textContent = new Date();
+timeBtn.onclick = function () {
+	mode = 'time';
+	update();
+};
+
+dateBtn.onclick = function () {
+	mode = 'date';
+	update();
+};
+
+fullBtn.onclick = function () {
+	mode = 'full';
+	update();
+};
+
+setInterval(update, 1000);
+update();
+
+function update() {
+	output.textContent = format(mode);
+}
+
+// Pure Function
+function format(formatMode) {
+	const now = new Date();
+	switch (formatMode) {
+		case 'time':
+			return now.toLocaleTimeString();
+		case 'date':
+			return now.toLocaleDateString();
+		case 'full':
+			return now.toLocaleDateString() + ' ' + now.toLocaleTimeString();
+		default:
+			return now.toLocaleTimeString();
+	}
+}
