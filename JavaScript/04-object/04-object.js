9a918f24ea50e201912f750cac16f6dc2b8fa916
Vladimir <komlevladimir@gmail.com>
2023-08-30 16:52:36 +0300

    add: 04-object.js task4-5

--- JavaScript/04-object/04-object.js
+++ JavaScript/04-object/04-object.js
@@ -156,12 +156,65 @@ console.log(person.friends);
 person.showFriends(); // Иван, Сергей, Игорь
 */
 
-
 /* 04-object task 4
- */
+ 
+class Animal {
+	constructor(name, favoriteFood) {
+		this.name = name;
+		this.favoriteFood = favoriteFood;
+	}
+	makeSound() {}
+	sayName() {
+		console.log(`My name is ${this.name}`);
+	}
+	sayInfo() {
+		console.log(`${this.name}' favorite food is ${this.favoriteFood}`);
+	}
+}
+
+class Dog extends Animal {
+	makeSound() {
+		console.log('Gav!');
+	}
+}
+
+class Cat extends Animal {
+	makeSound() {
+		console.log('Meow');
+	}
+}
+
+const dog = new Dog('Rex', 'Meat');
+// console.log(dog);
+const cat = new Cat('Barsik', 'Fish');
+// console.log(cat);
+
+cat.makeSound(); // Meow
+dog.makeSound(); // Gav!
+
+dog.sayName(); // My name is Rex
+cat.sayName(); // My name is Barsik
+
+dog.sayInfo(); // Rex's favorite food is Meat
+cat.sayInfo(); // Barsik's favorite food is Fish
+*/
 
 /* 04-object task 5
- */
+
+const foo = {
+	a: 5,
+	bar: function () {
+		console.log(this.a);
+	},
+	baz: () => {
+		console.log(this.a);
+	},
+};
+
+foo.bar(); // 5
+foo.baz(); // undefined т.к. обращается к глобальному this
+*/
+
 
 /* 04-object task 6
  */
